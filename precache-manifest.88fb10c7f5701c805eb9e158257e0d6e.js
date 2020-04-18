2272d80c8d14e4cbddab8bb931c8f3a9c5e6fe78
Abdullahi <abdullahihassanke@gmail.com>
2020-04-18 21:37:13 +0300

    Updates

---
+++ precache-manifest.88fb10c7f5701c805eb9e158257e0d6e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7fff574442fce50d9742007937997568",
+    "url": "/sothside_news_app/index.html"
+  },
+  {
+    "revision": "742ae114432842506086",
+    "url": "/sothside_news_app/static/css/2.69121389.chunk.css"
+  },
+  {
+    "revision": "0934c17158ce0f664213",
+    "url": "/sothside_news_app/static/css/main.f14790f6.chunk.css"
+  },
+  {
+    "revision": "742ae114432842506086",
+    "url": "/sothside_news_app/static/js/2.2fc0cb2e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/sothside_news_app/static/js/2.2fc0cb2e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0934c17158ce0f664213",
+    "url": "/sothside_news_app/static/js/main.4f6398ac.chunk.js"
+  },
+  {
+    "revision": "ef01c69d7555bbb6913b",
+    "url": "/sothside_news_app/static/js/runtime-main.830476d8.js"
+  },
+  {
+    "revision": "775733e15eb05fc133b11b02e8cc6c4d",
+    "url": "/sothside_news_app/static/media/logo.775733e1.png"
+  }
+]);
